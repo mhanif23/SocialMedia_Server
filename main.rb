a7a4c65b88dd26c2ed9ab739a004850ac899a174
rizalraisyad <muharamrizal2@gmail.com>
2021-08-19 22:06:41 +0700

    [Initialize] init main.rb as a server with test api get, post json, and file form data with text

---
+++ main.rb
@@ -0,0 +1,41 @@
+  
+require "sinatra"
+require "sinatra/json"
+require "json"
+
+before do
+  content_type :json
+end
+
+get "/test" do
+  status 200
+  json({
+    status: 200,
+    message: "Success!",
+    data: {}
+  })
+end
+
+post "/test" do
+  request_payload = JSON.parse request.body.read
+  puts request_payload
+  status 200
+  json({
+    status: 200,
+    message: "Success!",
+    data: request_payload
+  })
+end
+
+
+
+post "/file" do
+  tmpfile = params[:file]
+  puts tmpfile
+  status 200
+  json({
+    status: 200,
+    message: "Success!",
+    data: tmpfile
+  })
+end
