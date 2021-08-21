f35641c08b8313f3704d121160328308081e2e3d
rizalraisyad <muharamrizal2@gmail.com>
2021-08-21 13:16:13 +0700

    [TEST_GREEN] Find Trending

--- models/hastag.rb
+++ models/hastag.rb
@@ -1,14 +1,22 @@
 require_relative "../connector/db_connector"
 
-
-class Hashtag
-  attr_accessor :name, :hastag_used
+class Hastags
+  attr_accessor :hastag
   
   def initialize(params)
-    @name = params[:name]
+    @hastag = params[:hastag]
   end
 
-  def self.find_trending
+  def self.find_trending()
+    client = create_db_client
 
+    rows = client.query('select Hastags.hastag, COUNT(id_hastag) as total_hastag FROM Hastag_contracts inner join Hastags on id_hastag = Hastags.id WHERE createdAt > (NOW() - INTERVAL 1 DAY)  GROUP BY hastag ORDER BY count(id_hastag) DESC LIMIT 5')
+    hastags = []
+
+    rows.each do |row|
+      hastag = Hastags.new(hastag: row['hastag'])
+      hastags << hastag
+    end
+    hastags
   end
 end
--- spec/models/hastag_spec.rb
+++ spec/models/hastag_spec.rb
@@ -1,26 +1,33 @@
 require_relative "../../models/hastag.rb"
 
-describe Hashtag do
+describe Hastags do
   before :each do
     @client = double
     allow(Mysql2::Client).to receive(:new).and_return(@client)
   end
   describe 'initialize' do
     before :each do
-      @hashtag_data = {name: '#liverpool'}
-      @hashtag = Hashtag.new(
-        name: @hashtag_data[:name]
+      @hastag_data = {hastag: '#liverpool'}
+      @hastag = Hastags.new(
+        hastag: @hastag_data[:hastag]
       )
     end
     it 'should give name attribute' do
-      expect(@hashtag.name).to be(@hashtag_data[:name])
+      expect(@hastag.hastag).to be(@hastag_data[:hastag])
     end
   end
 
   describe 'get trending' do
     it 'should exec query' do
       query = "select Hastags.hastag, COUNT(id_hastag) as total_hastag FROM Hastag_contracts inner join Hastags on id_hastag = Hastags.id WHERE createdAt > (NOW() - INTERVAL 1 DAY)  GROUP BY hastag ORDER BY count(id_hastag) DESC LIMIT 5"
-      expect(@client).to receive(:query).with(query)
+      expect(@client).to receive(:query).with(query).and_return([])
+      expect(Hastags::find_trending()).to eq([])
+    end
+    it 'give object' do
+      query = "select Hastags.hastag, COUNT(id_hastag) as total_hastag FROM Hastag_contracts inner join Hastags on id_hastag = Hastags.id WHERE createdAt > (NOW() - INTERVAL 1 DAY)  GROUP BY hastag ORDER BY count(id_hastag) DESC LIMIT 5"
+      data = [{"hastag"=> "test"}]
+      expect(@client).to receive(:query).with(query).and_return(data)
+      expect(Hastags::find_trending().length).to eq(data.length)
     end
   end
 end
